1dd3db07bdecb4d03d6bc5a0f25e80215545142a
xDiaym <matthew.nekirov@gmail.com>
2022-01-07 19:16:39 +0300

    build: fix import order error

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import numpy
 from setuptools import setup
-from Cython.Build import cythonize
+
+from Cython.Build import cythonize  # isort: skip strange build system bug
 
 
 def read(filename: str) -> str:
-    with open(filename, "r") as fp:
+    with open(filename, "r", encoding="utf8") as fp:
         return fp.read()
 
 
 setup(
     name="poom",
     description=read("README.md"),
-    ext_modules=cythonize("poom/ray_march.pyx", language_level="3", gdb_debug=True),
+    ext_modules=cythonize("poom/ray_march.pyx", language_level="3"),
     include_dirs=numpy.get_include(),
 )
