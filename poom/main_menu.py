3d84d79f00badb07763bc70686d92a18038866a2
BazaroZero <finatov05@gmail.com>
2022-01-08 22:58:25 +0300

    refactor(menu)

--- poom/main_menu.py
+++ poom/main_menu.py
@@ -2,68 +2,125 @@ from abc import ABC, abstractmethod
 from os import getcwd
 from pathlib import Path
 
-import pygame
+import pygame as pg
 import pygame_gui
+from pygame_gui.elements import (
+    UILabel,
+    UIButton,
+    UIDropDownMenu,
+    UIHorizontalSlider,
+    UITextBox,
+)
+from pygame_gui.core import ObjectID
+from os import listdir
 
 # "#2f353b"
-pygame.init()
+pg.init()
 size = width, height = 800, 600
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Greeting")
+screen = pg.display.set_mode(size)
+pg.display.set_caption("Greeting")
 root = Path(getcwd())
-background = pygame.transform.scale(
-    pygame.image.load(root / "assets" / "back.png").convert_alpha(), (width, height)
+background = pg.transform.scale(
+    pg.image.load(root / "assets" / "back.png").convert_alpha(), (width, height)
 )
 running = True
-clock = pygame.time.Clock()
-manager = pygame_gui.UIManager(size, root / "assets" / "style.json")
-animation = pygame.sprite.GroupSingle()
+clock = pg.time.Clock()
 
 
-class Zombie(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__(animation)
-        self.width, self.height = 111, 162
-        self.images = [
-            pygame.transform.scale(
-                pygame.image.load(
-                    root / "assets" / "zombie" / f"{i}.png"
-                ).convert_alpha(),
-                (self.width, self.height),
+def clamp(low, high, value):
+    return max(min(high, value), low)
+
+
+class SingletonManager(pygame_gui.UIManager):
+    _instances = {}
+
+    def __call__(cls, size, style_dir):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonManager, cls).__call__(size, style_dir)
+        return cls._instances[cls]
+
+
+manager = SingletonManager(size, root / "assets" / "style.json")
+
+
+class Animation:
+    def __init__(self, images: list, speed: float) -> None:
+        self._images = images
+        self._speed = speed
+        self._animation_rate = 0
+
+    def update(self, dt) -> None:
+        self._animation_rate += dt * self._speed
+
+    def flip_images(self) -> None:
+        for i in range(len(self._images)):
+            self._images[i] = pg.transform.flip(self._images[i], True, False)
+
+    def reset(self) -> None:
+        self._animation_rate = 0
+
+    @property
+    def done(self) -> float:
+        return self._animation_rate > len(self._images)
+
+    @property
+    def current_frame(self) -> pg.Surface:
+        return self._images[int(self._animation_rate) % len(self._images)]
+
+    @classmethod
+    def from_dir(cls, root: Path, speed: float, scale: float = 1):
+        filenames = sorted(listdir(root), key=lambda x: int(Path(x).stem))
+        images = []
+        for name in filenames:
+            path = root / name
+            source = pg.image.load(path).convert_alpha()
+            images.append(
+                pg.transform.scale(
+                    source,
+                    (int(source.get_width() * scale), int(source.get_height() * scale)),
+                )
             )
-            for i in range(5)
-        ]
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.rect.x = width - self.width
-        self.rect.y = height - self.height
+        return cls(images, speed)
+
+
+class Zombie(pg.sprite.Sprite):
+    def __init__(self) -> None:
+        self.group = pg.sprite.GroupSingle()
+        super().__init__(self.group)
         self.v = 30
-        self.x = width - self.width
-        self.k_rotates = 0
+        self.walking_animation = Animation.from_dir(
+            root / "assets" / "zombie" / "walk", 3, 0.1875
+        )
+        self.rotation_animation = Animation.from_dir(
+            root / "assets" / "zombie" / "rotate", 3, 0.1875
+        )
+        self.image = self.walking_animation.current_frame
+        self.rect = self.image.get_rect()
+        self.rect.x = width - self.image.get_width()
+        self.rect.y = height - self.image.get_height()
+        self.x = self.rect.x
         self.rotate = False
         self.left = True
-        self.animation_rate = 0
 
-    def update(self, dt):
-        self.x -= self.v * dt
-        self.rect.x = self.x
-        self.animation_rate += 3 * dt
-        if not 0 < self.x <= width - self.width and not self.rotate:
-            self.v = 0
+    def update(self, dt) -> None:
+        if not 0 < self.x <= width - self.image.get_width() and not self.rotate:
             self.rotate = True
-            self.k_rotates = self.animation_rate
+            self.rotation_animation.reset()
         if not self.rotate:
-            self.image = self.images[int(self.animation_rate) % 2]
+            self.x -= self.v * dt
+            self.rect.x = self.x
+            self.walking_animation.update(dt)
+            self.image = self.walking_animation.current_frame
         else:
-            if self.animation_rate - self.k_rotates < 3:
-                self.image = self.images[2 + int(self.animation_rate - self.k_rotates)]
+            if not self.rotation_animation.done:
+                self.rotation_animation.update(dt)
+                self.image = self.rotation_animation.current_frame
             else:
-                for i in range(5):
-                    self.images[i] = pygame.transform.flip(self.images[i], True, False)
-                self.v = -30 if self.left else 30
-                self.x += 2 if self.left else -2
+                self.walking_animation.flip_images()
+                self.rotation_animation.flip_images()
+                self.v = -self.v
+                self.x = clamp(1, width - self.image.get_width() - 1, self.x)
                 self.left = not self.left
-                self.k_rotates = 0
                 self.rotate = False
 
 
@@ -75,59 +132,63 @@ class AbstractScene(ABC):
     def on_click(self, event) -> None:
         pass
 
+    @abstractmethod
+    def update(self, dt: float) -> None:
+        pass
+
+    @abstractmethod
+    def render(self, render: pg.Surface) -> None:
+        pass
+
 
 class SceneContext:
     def __init__(self) -> None:
-        self._scene = WelcomeScene()
-        self.zombie = Zombie()
+        self._scene = WelcomeScene(self)
 
     @property
-    def scene(self):
+    def scene(self) -> AbstractScene:
         return self._scene
 
     @scene.setter
-    def scene(self, value):
+    def scene(self, value: AbstractScene) -> None:
         self._scene = value
 
-    def handler(self, event):
+    def handler(self, event) -> None:
         self._scene.on_click(event)
 
-    def move_animation(self):
-        animation.draw(screen)
-        animation.update(clock.tick() / 1000)
-
-    def render(self):
+    def render(self) -> None:
         screen.blit(background, (0, 0))
-        self.move_animation()
-        manager.update(0)
-        manager.draw_ui(screen)
+        self._scene.render(screen)
+        self._scene.update(clock.tick() / 1000)
 
 
-class WelcomeScene:
-    def __init__(self) -> None:
-        self.poom_label = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 220) // 2, height * 0.05, 220, 110),
+class WelcomeScene(AbstractScene):
+    def __init__(self, context, zombie=Zombie()) -> None:
+        super().__init__(context)
+        self.zombie = zombie
+        self.poom_label = UILabel(
+            pg.Rect((width - 220) // 2, height * 0.05, 220, 110),
             "Poom",
             manager,
-            object_id=pygame_gui.core.ui_element.ObjectID(object_id="#poom"),
+            object_id=ObjectID(object_id="#poom"),
         )
-        self.play = pygame_gui.elements.UIButton(
-            pygame.Rect((width - 160) // 2, height * 0.3, 160, 70),
+        self.play = UIButton(
+            pg.Rect((width - 160) // 2, height * 0.3, 160, 70),
             "Play",
             manager,
         )
-        self.settings = pygame_gui.elements.UIButton(
-            pygame.Rect((width - 160) // 2, height * 0.4, 160, 70),
+        self.settings = UIButton(
+            pg.Rect((width - 160) // 2, height * 0.4, 160, 70),
             "Settings",
             manager,
         )
-        self.statistics = pygame_gui.elements.UIButton(
-            pygame.Rect((width - 160) // 2, height * 0.5, 160, 70),
+        self.statistics = UIButton(
+            pg.Rect((width - 160) // 2, height * 0.5, 160, 70),
             "Statistics",
             manager,
         )
-        self.quit = pygame_gui.elements.UIButton(
-            pygame.Rect((width - 160) // 2, height * 0.6, 160, 70),
+        self.quit = UIButton(
+            pg.Rect((width - 160) // 2, height * 0.6, 160, 70),
             "Quit",
             manager,
         )
@@ -138,77 +199,87 @@ class WelcomeScene:
             if event.ui_element == self.play:
                 print("play")
             if event.ui_element == self.settings:
-                sc.scene = SettingsScene()
+                self._context.scene = SettingsScene(self._context, self.zombie)
             if event.ui_element == self.statistics:
-                sc.scene = StatiscticsScene()
+                self._context.scene = StatiscticsScene(self._context, self.zombie)
             if event.ui_element == self.quit:
                 print("quit")
 
+    def render(self, surface: pg.Surface) -> None:
+        self.zombie.group.draw(surface)
+        manager.draw_ui(surface)
 
-class SettingsScene:
-    def __init__(self) -> None:
-        self.settings_label = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 230) // 2, height * 0.05, 230, 80),
+    def update(self, dt: float) -> None:
+        self.zombie.group.update(dt)
+        manager.update(0)
+
+
+class SettingsScene(AbstractScene):
+    def __init__(self, context, zombie: Zombie) -> None:
+        super().__init__(context)
+        self.zombie = zombie
+        self.settings_label = UILabel(
+            pg.Rect((width - 230) // 2, height * 0.05, 230, 80),
             "Settings",
             manager,
         )
-        self.back = pygame_gui.elements.UIButton(
-            pygame.Rect(width * 0.05, height * 0.075, 50, 50),
+        self.back = UIButton(
+            pg.Rect(width * 0.05, height * 0.075, 50, 50),
             "X",
             manager,
         )
-        self.graphics_lbl = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 200) // 2, height * 0.23, 100, 50),
+        self.graphics_lbl = UILabel(
+            pg.Rect((width - 200) // 2, height * 0.23, 100, 50),
             "Quality:",
             manager,
-            object_id=pygame_gui.core.ui_element.ObjectID(object_id="#sublabel"),
+            object_id=ObjectID(object_id="#sublabel"),
         )
-        self.graphics = pygame_gui.elements.UIDropDownMenu(
+        self.graphics = UIDropDownMenu(
             ["Low", "Medium", "High"],
             "Low",
-            pygame.Rect((width - 200) // 2, height * 0.3, 200, 50),
+            pg.Rect((width - 200) // 2, height * 0.3, 200, 50),
             manager,
         )
-        self.screen_size_lbl = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 200) // 2, height * 0.4, 150, 50),
+        self.screen_size_lbl = UILabel(
+            pg.Rect((width - 200) // 2, height * 0.4, 150, 50),
             "Screen size:",
             manager,
-            object_id=pygame_gui.core.ui_element.ObjectID(object_id="#sublabel"),
+            object_id=ObjectID(object_id="#sublabel"),
         )
-        self.screen_size = pygame_gui.elements.UIDropDownMenu(
+        self.screen_size = UIDropDownMenu(
             ["800x600 (4:3)", "1024x768 (4:3)", "1280x720 (16:9)"],
             "800x600 (4:3)",
-            pygame.Rect((width - 200) // 2, height * 0.47, 290, 50),
+            pg.Rect((width - 200) // 2, height * 0.47, 290, 50),
             manager,
         )
-        self.volume_lbl = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 200) // 2, height * 0.58, 100, 50),
+        self.volume_lbl = UILabel(
+            pg.Rect((width - 200) // 2, height * 0.58, 100, 50),
             "Volume:",
             manager,
-            object_id=pygame_gui.core.ui_element.ObjectID(object_id="#sublabel"),
+            object_id=ObjectID(object_id="#sublabel"),
         )
-        self.volume = pygame_gui.elements.UIHorizontalSlider(
-            pygame.Rect((width - 200) // 2, height * 0.65, 270, 30),
+        self.volume = UIHorizontalSlider(
+            pg.Rect((width - 200) // 2, height * 0.65, 270, 30),
             0.5,
             range(101),
             manager,
         )
-        self.current_volume = pygame_gui.elements.UILabel(
-            pygame.Rect(width * 0.7, height * 0.635, 100, 50),
+        self.current_volume = UILabel(
+            pg.Rect(width * 0.7, height * 0.635, 100, 50),
             "50 %",
             manager,
-            object_id=pygame_gui.core.ui_element.ObjectID(object_id="#sublabel"),
+            object_id=ObjectID(object_id="#sublabel"),
         )
-        self.fps_lbl = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 200) // 2, height * 0.71, 100, 50),
+        self.fps_lbl = UILabel(
+            pg.Rect((width - 200) // 2, height * 0.71, 100, 50),
             "Fps tick:",
             manager,
-            object_id=pygame_gui.core.ui_element.ObjectID(object_id="#sublabel"),
+            object_id=ObjectID(object_id="#sublabel"),
         )
-        self.fps = pygame_gui.elements.UIDropDownMenu(
+        self.fps = UIDropDownMenu(
             ["on", "off"],
             "off",
-            pygame.Rect((width - 200) // 2, height * 0.78, 150, 50),
+            pg.Rect((width - 200) // 2, height * 0.78, 150, 50),
             manager,
         )
 
@@ -218,24 +289,34 @@ class SettingsScene:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == self.back:
                 manager.clear_and_reset()
-                sc.scene = WelcomeScene()
+                self._context.scene = WelcomeScene(self._context, self.zombie)
+
+    def render(self, surface: pg.Surface) -> None:
+        self.zombie.group.draw(surface)
+        manager.draw_ui(surface)
+
+    def update(self, dt: float) -> None:
+        self.zombie.group.update(dt)
+        manager.update(0)
 
 
-class StatiscticsScene:
-    def __init__(self) -> None:
-        self.stats_label = pygame_gui.elements.UILabel(
-            pygame.Rect((width - 230) // 2, height * 0.05, 230, 80),
+class StatiscticsScene(AbstractScene):
+    def __init__(self, context, zombie: Zombie) -> None:
+        super().__init__(context)
+        self.zombie = zombie
+        self.stats_label = UILabel(
+            pg.Rect((width - 230) // 2, height * 0.05, 230, 80),
             "Statistics",
             manager,
         )
-        self.back = pygame_gui.elements.UIButton(
-            pygame.Rect(width * 0.05, height * 0.075, 50, 50),
+        self.back = UIButton(
+            pg.Rect(width * 0.05, height * 0.075, 50, 50),
             "X",
             manager,
         )
-        self.stats = pygame_gui.elements.UITextBox(
+        self.stats = UITextBox(
             "1 level:<br>    Kills: 1023<br>    Damage: 800",
-            pygame.Rect(width * 0.2, height * 0.3, width * 0.6, height * 0.5),
+            pg.Rect(width * 0.2, height * 0.3, width * 0.6, height * 0.5),
             manager,
         )
 
@@ -243,25 +324,33 @@ class StatiscticsScene:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == self.back:
                 manager.clear_and_reset()
-                sc.scene = WelcomeScene()
+                self._context.scene = WelcomeScene(self._context, self.zombie)
+
+    def render(self, surface: pg.Surface) -> None:
+        self.zombie.group.draw(surface)
+        manager.draw_ui(surface)
+
+    def update(self, dt: float) -> None:
+        self.zombie.group.update(dt)
+        manager.update(0)
 
 
 sc = SceneContext()
 
-pygame.mixer.init()
-pygame.mixer.music.load(root / "assets" / "main.mp3")
-pygame.mixer.music.play(-1)
+pg.mixer.init()
+pg.mixer.music.load(root / "assets" / "main.mp3")
+pg.mixer.music.play(-1)
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-        if event.type == pygame.WINDOWMINIMIZED:
-            pygame.mixer.music.pause()
-        if event.type == pygame.WINDOWRESTORED:
-            pygame.mixer.music.unpause()
+        if event.type == pg.WINDOWMINIMIZED:
+            pg.mixer.music.pause()
+        if event.type == pg.WINDOWRESTORED:
+            pg.mixer.music.unpause()
         sc.handler(event)
         manager.process_events(event)
     sc.render()
 
-    pygame.display.update()
+    pg.display.update()
